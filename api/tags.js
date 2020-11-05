d1e16254599bce2c7b83cc2b0e17748c5b89b861
MarioP01 <mario.perez.learning@gmail.com>
2020-11-04 21:57:25 -0500

    Testing 3 Practice

--- api/tags.js
+++ api/tags.js
@@ -21,4 +21,28 @@ tagsRouter.get("/", async (req, res) => {
   }
 });
 
+tagsRouter.get("/:tagName/posts", async (req, res) => {
+  const { tagName } = req.params;
+
+  try {
+    const allPosts = await getPostsByTagName(tagName);
+
+    const posts = allPosts.filter((post) => {
+      if (post.active) {
+        return true;
+      }
+
+      if (req.user && req.user.id === post.author.id) {
+        return true;
+      }
+
+      return false;
+    });
+
+    res.send({ posts });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 module.exports = tagsRouter;
